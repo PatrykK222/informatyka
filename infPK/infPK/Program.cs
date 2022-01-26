390623531df675cfa62093be83653c5c20f7a2db
DESKTOP-6UP8NFB\adminzdz <patrykkusmierczyk09@gmailaaa.com>
2022-01-26 09:29:44 +0100

    Added new c++ project with the gitignore

---
+++ infPK/infPK/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace infPK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
